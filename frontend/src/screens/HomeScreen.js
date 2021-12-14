7ee4ffc4e6aa924a640125c762967e50eaeea8e7
thongdinh <dt96dn@gmail.com>
2021-12-14 20:59:21 +0700

    Initial commit.

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Row, Col } from 'react-bootstrap'
+import Product from '../components/Product'
+
+const HomeScreen = () => {
+  const dispatch = useDispatch()
+  
+  useEffect(() => {}, [])
+
+  return (
+    <>
+      <h1>Lastest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
+}
+
+export default HomeScreen
