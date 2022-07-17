edb6ca057adf9d5d76a363abc55da5d7bc3d000a
thongdinh <dt96dn@gmail.com>
2022-07-17 11:16:48 +0700

    Update user profile

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -125,3 +125,37 @@ export const getUserDetails = (id) => async (dispatch, getState) => {
     })
   }
 }
+
+export const updateUserProfile = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_UPDATE_PROFILE_REQUEST,
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.put(`/api/users/profile`, user, config)
+
+    dispatch({
+      type: USER_UPDATE_PROFILE_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: USER_UPDATE_PROFILE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
